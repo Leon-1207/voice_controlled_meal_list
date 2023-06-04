d64d45101277a91bf86acb4bd58dbc93ee6f374d
Dimitri Williams <82943582+retroZap@users.noreply.github.com>
2023-06-04 18:59:44 +0200

    Added Button Animation

--- app/src/main/java/com/bueroservice/spechtest/MainActivity.java
+++ app/src/main/java/com/bueroservice/spechtest/MainActivity.java
@@ -22,14 +22,17 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     private static final int SPEECH_REQUEST_CODE = 123;
-    private static final List<String> possiblePersonNames = new ArrayList<>(Arrays.asList("Tim", "Max", "Tina", "Fareed", "Moayad", "Dimitri", "Leon"));
-    private static final List<String> possibleFood = new ArrayList<>(Arrays.asList("Fleisch", "Vegetarisch", "Vegan", "Lite"));
+    private static final List<String> possiblePersonNames = new ArrayList<>(
+            Arrays.asList("Tim", "Max", "Tina", "Fareed", "Moayad", "Dimitri", "Leon"));
+    private static final List<String> possibleFood = new ArrayList<>(
+            Arrays.asList("Fleisch", "Vegetarisch", "Vegan", "Lite"));
 
     private TableLayout entryTable;
     private TextToSpeechUtil textToSpeech;
 
-    // Later it has to be Map<String (Name der Person), Map<Date, String (Gericht)>) to store the selection for multiple dates
-    private Map<String, String> entries;   // Map<String (Name der Person), String (Gericht)>
+    // Later it has to be Map<String (Name der Person), Map<Date, String (Gericht)>)
+    // to store the selection for multiple dates
+    private Map<String, String> entries; // Map<String (Name der Person), String (Gericht)>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,16 +50,50 @@ public class MainActivity extends AppCompatActivity {
                 startSpeechRecognition();
             }
         });
+
+        Animation anim = new ScaleAnimation(
+                1.0f, 1.2f, // Start- und Endskalierungsfaktor
+                1.0f, 1.2f, // Start- und Endskalierungsfaktor
+                Animation.RELATIVE_TO_SELF, 0.5f, // Skalierung um die X-Achse
+                Animation.RELATIVE_TO_SELF, 0.5f // Skalierung um die Y-Achse
+        );
+        anim.setDuration(1000); // Setzen Sie hier die gewünschte Pulsdauer in Millisekunden ein
+        anim.setInterpolator(new AccelerateDecelerateInterpolator());
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setRepeatMode(Animation.REVERSE);
+        addEntryButton.startAnimation(anim);
     }
 
     private void startSpeechRecognition() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Sprechen Sie Ihre gewünschte Person, das Datum und das Essen ein.");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Sprechen Sie Ihre gewünschte Person, das Datum und das Essen ein.");
 
         // increase recording duration
-        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, "5000");    // The amount of time that it should take after we stop hearing speech to consider the input complete.
-        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, "5000"); //  The amount of time that it should take after we stop hearing speech to consider the input possibly complete.
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS, "5000"); // The amount of
+                                                                                                     // time that it
+                                                                                                     // should take
+                                                                                                     // after we stop
+                                                                                                     // hearing speech
+                                                                                                     // to consider the
+                                                                                                     // input complete.
+        intent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_POSSIBLY_COMPLETE_SILENCE_LENGTH_MILLIS, "5000"); // The
+                                                                                                              // amount
+                                                                                                              // of time
+                                                                                                              // that it
+                                                                                                              // should
+                                                                                                              // take
+                                                                                                              // after
+                                                                                                              // we stop
+                                                                                                              // hearing
+                                                                                                              // speech
+                                                                                                              // to
+                                                                                                              // consider
+                                                                                                              // the
+                                                                                                              // input
+                                                                                                              // possibly
+                                                                                                              // complete.
 
         try {
             startActivityForResult(intent, SPEECH_REQUEST_CODE);
@@ -102,14 +139,17 @@ public class MainActivity extends AppCompatActivity {
                                 break;
                             }
                         }
-                    } else break;
+                    } else
+                        break;
                 }
 
                 if (!personFound) {
-                    // the person was not found --> search again for food to give feedback about food
+                    // the person was not found --> search again for food to give feedback about
+                    // food
                     for (String word : entryParts) {
                         // search for food
-                        if (foodFound) break;
+                        if (foodFound)
+                            break;
                         for (String possibleFoodValue : possibleFood) {
                             if (word.equalsIgnoreCase(possibleFoodValue)) {
                                 // food was found
@@ -167,8 +207,8 @@ public class MainActivity extends AppCompatActivity {
         for (String person : entries.keySet()) {
             String food = entries.get(person);
             TableRow row = new TableRow(this);
-            TextView personTextView = createTextView(person);   // TextView for name of person
-            TextView foodTextView = createTextView(food);       // TextView for food choice of person
+            TextView personTextView = createTextView(person); // TextView for name of person
+            TextView foodTextView = createTextView(food); // TextView for food choice of person
             row.addView(personTextView);
             row.addView(foodTextView);
             entryTable.addView(row);
